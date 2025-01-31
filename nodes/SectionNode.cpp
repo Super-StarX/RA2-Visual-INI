75a857216d87cfd1d20d98b97537f79a5efe1ffd
Uranusian <mikesariitten@gmail.com>
2025-01-31 19:09:09 +0800

    SectionNode再修

--- nodes/SectionNode.cpp
+++ nodes/SectionNode.cpp
@@ -11,33 +11,28 @@ void SectionNode::Update() {
 	ImGui::TextUnformatted(this->Name.c_str());
 	ImGui::Spring(1);
 	ImGui::Dummy(ImVec2(0, 28));
+	ImGui::Spring(0);
 	builder->EndHeader();
 
 	// 渲染键值对
 	for (auto& kv : this->KeyValues) {
+		auto alpha = kv.OutputPin.GetAlpha(Owner->newLinkPin);
+		ImGui::PushStyleVar(ImGuiStyleVar_Alpha, alpha);
 		ImGui::PushID(&kv);
+		builder->Output(kv.OutputPin.ID);
 
-		ImGui::BeginGroup();
 		ImGui::SetNextItemWidth(80);
 		ImGui::InputText("##Key", &kv.Key);
-		ImGui::EndGroup();
 
-		ImGui::BeginGroup();
 		ImGui::SetNextItemWidth(120);
 		ImGui::InputText("##Value", &kv.Value, ImGuiInputTextFlags_ReadOnly);
-		ImGui::EndGroup();
 
-		ImGui::BeginGroup();
-		ImGui::SetCursorPosY(ImGui::GetCursorPosY() + 3);
-
-		float alpha = kv.OutputPin.GetAlpha(Owner->newLinkPin);
-		ed::BeginPin(kv.OutputPin.ID, ed::PinKind::Output);
+		ImGui::Spring(0);
 		kv.OutputPin.DrawPinIcon(Owner->IsPinLinked(kv.OutputPin.ID), (int)(alpha * 255));
-		ed::EndPin();
-		ImGui::EndGroup();
 
+		builder->EndOutput();
 		ImGui::PopID();
-		ImGui::Dummy(ImVec2(0, 28));
+		ImGui::PopStyleVar();
 	}
 
 	builder->End();
