616103d1d098ec0fbcd670126660d69123d63a6c
小星星 <88648845@qq.com>
2025-03-13 00:52:01 +0800

    IONode

---
+++ nodes/IONode.cpp
@@ -0,0 +1,24 @@
+﻿
+#include "IONode.h"
+
+IONode::IONode(const char* name, IOMode mode, int id)
+	: Node(name, id), mode(mode) {
+	Color = (mode == IOMode::Input) ? ImColor(0, 255, 0) : ImColor(255, 0, 0);
+
+	if (mode == IOMode::Input) {
+		IOPin = std::make_unique<Pin>(this, "input", PinKind::Input);
+	}
+	else {
+		IOPin = std::make_unique<Pin>(this, "output", PinKind::Output);
+	}
+}
+
+void IONode::SaveToJson(json& j) const {
+	Node::SaveToJson(j);
+	j["Mode"] = (mode == IOMode::Input) ? "Input" : "Output";
+}
+
+void IONode::LoadFromJson(const json& j) {
+	Node::LoadFromJson(j);
+	mode = (j["Mode"] == "Input") ? IOMode::Input : IOMode::Output;
+}
---
+++ nodes/IONode.h
@@ -0,0 +1,22 @@
+﻿// IONode.h
+#pragma once
+#include "Node.h"
+
+enum class IOMode { Input, Output };
+
+class IONode : public Node {
+public:
+	IONode(const char* name, IOMode mode, int id = 0);
+	virtual NodeType GetNodeType() const override {
+		return mode == IOMode::Input ? NodeType::Input : NodeType::Output;
+	}
+	virtual void SaveToJson(json& j) const override;
+	virtual void LoadFromJson(const json& j) override;
+
+	IOMode GetMode() const { return mode; }
+	Pin* GetPin() const { return IOPin.get(); };
+
+private:
+	IOMode mode;
+	std::unique_ptr<Pin> IOPin;
+};
